09590586e1815b973516d96149261d6383f01c1b
Patryk <patryk.ciwinski@gmail.com>
2022-11-04 10:53:26 +0100

    Madlibs_python

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+
+
+adj=str(input("type in adjective: "))
+verb1=str(input("type in verb: "))
+adj2=str(input("type in second adjective: "))
+verb2=str(input("type in  second verb: "))
+famous_person=str(input("type in famous person: "))
+
+madlibs=f"programming is so {adj}: it makes me so crazy and {verb1} I love to be {adj2}. Stay tuned and {verb2}, be like {famous_person}"
+
+print (madlibs)
